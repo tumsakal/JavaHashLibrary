937142fb0325c4175facf852f55d21f90f779f78
sakal <tumsakal.ts@gmail.com>
2019-11-06 10:57:59 +0700

    Remove Padding

--- src/ant/util/security/Hash.java
+++ src/ant/util/security/Hash.java
@@ -17,35 +17,35 @@ public class Hash {
 
     public static String computeBase64MD5(byte[] data) {
         byte[] md5 = computeHash("MD5", data);
-        return Base64.getEncoder().encodeToString(md5);
+        return Base64.getEncoder().withoutPadding().encodeToString(md5);
     }
 
     public static String computeBase64MD5(String data) {
         byte[] rawData = data.getBytes();
         byte[] md5 = computeHash("MD5", rawData);
 
-        return Base64.getEncoder().encodeToString(md5);
+        return Base64.getEncoder().withoutPadding().encodeToString(md5);
     }
 
     public static String computeBase64SHA1(byte[] data) {
         byte[] sha1 = computeHash("SHA-1", data);
-        return Base64.getEncoder().encodeToString(sha1);
+        return Base64.getEncoder().withoutPadding().encodeToString(sha1);
     }
 
     public static String computeBase64SHA1(String data) {
         byte[] rawData = data.getBytes();
         byte[] sha1 = computeHash("SHA-1", rawData);
-        return Base64.getEncoder().encodeToString(sha1);
+        return Base64.getEncoder().withoutPadding().encodeToString(sha1);
     }
 
     public static String computeBase64SHA256(byte[] data) {
         byte[] sha256 = computeHash("SHA-256", data);
-        return Base64.getEncoder().encodeToString(sha256);
+        return Base64.getEncoder().withoutPadding().encodeToString(sha256);
     }
 
     public static String computeBase64SHA256(String data) {
         byte[] rawData = data.getBytes();
         byte[] sha256 = computeHash("SHA-256", rawData);
-        return Base64.getEncoder().encodeToString(sha256);
+        return Base64.getEncoder().withoutPadding().encodeToString(sha256);
     }
 }
